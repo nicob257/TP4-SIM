05837af9e58abf54d1c700ceb4f253a2c9d5b19b
Nicolas Basta <90347@sistemas.frc.utn.edu.ar>
2024-06-26 16:15:22 -0300

    No valida que tenga pelo (-20)

--- SistemasDinamicos/SistemasDinamicos/Form1.cs
+++ SistemasDinamicos/SistemasDinamicos/Form1.cs
@@ -16,22 +16,70 @@ namespace RelojeriaSimulacion
 
         private void btnSimular_Click(object sender, EventArgs e)
         {
-            simulator.InicializarProbabilidades(Convert.ToDouble(txtProbCompra.Text), Convert.ToDouble(txtProbEntrega.Text),
-                Convert.ToInt32(txtLiLleg.Text), Convert.ToInt32(txtLsLleg.Text), 
-                Convert.ToInt32(txtLiCp.Text), Convert.ToInt32(txtLsCp.Text), Convert.ToInt32(txtLiRep.Text), 
-                Convert.ToInt32(txtLsRep.Text), Convert.ToInt32(txtTmpOrden.Text),
-                Convert.ToInt32(txtLiInsp.Text), Convert.ToInt32(txtLsInsp.Text));
+            try
+            {
+                if (
+                Convert.ToDouble(txtProbCompra.Text) >= 0 &&
+                Convert.ToDouble(txtProbEntrega.Text) >= 0 &&
+                Convert.ToDouble(txtProbRetiro.Text) >= 0 &&
+                (Convert.ToDouble(txtProbRetiro.Text) + Convert.ToDouble(txtProbEntrega.Text) + Convert.ToDouble(txtProbCompra.Text) == 1) &&
+                Convert.ToInt32(txtLiLleg.Text) >= 0 &&
+                Convert.ToInt32(txtLsLleg.Text) >= 0 &&
+                (Convert.ToInt32(txtLiLleg.Text) <= Convert.ToInt32(txtLsLleg.Text)) &&
+                Convert.ToInt32(txtLiCp.Text) >= 0 &&
+                Convert.ToInt32(txtLsCp.Text) >= 0 &&
+                (Convert.ToInt32(txtLiCp.Text) <= Convert.ToInt32(txtLsCp.Text)) &&
+                Convert.ToInt32(txtLiRep.Text) >= 0 &&
+                Convert.ToInt32(txtLsRep.Text) >= 0 &&
+                (Convert.ToInt32(txtLiRep.Text) <= Convert.ToInt32(txtLsRep.Text)) &&
+                Convert.ToInt32(txtTmpOrden.Text) >= 0 &&
+                Convert.ToInt32(txtLiInsp.Text) >= 0 &&
+                Convert.ToInt32(txtLsInsp.Text) >= 0 &&
+                (Convert.ToInt32(txtLiInsp.Text) <= Convert.ToInt32(txtLsInsp.Text)) &&
+                Convert.ToInt32(txtRk1.Text) >= 0 &&
+                Convert.ToDouble(txtRk2.Text) >= 0 &&
+                Convert.ToInt32(txtRk3.Text) >= 0 &&
+                Convert.ToDouble(txtHRK.Text) >= 0 &&
+                Convert.ToInt32(tiempo.Text) >= 0 &&
+                Convert.ToInt32(iteraciones.Text) >= 0 &&
+                Convert.ToInt32(i.Text) >= 0 &&
+                Convert.ToInt32(j.Text) >= 0
+                )
+                {
+                    simulator.InicializarProbabilidades(Convert.ToDouble(txtProbCompra.Text), Convert.ToDouble(txtProbEntrega.Text),
+                    Convert.ToInt32(txtLiLleg.Text), Convert.ToInt32(txtLsLleg.Text),
+                    Convert.ToInt32(txtLiCp.Text), Convert.ToInt32(txtLsCp.Text), Convert.ToInt32(txtLiRep.Text),
+                    Convert.ToInt32(txtLsRep.Text), Convert.ToInt32(txtTmpOrden.Text),
+                    Convert.ToInt32(txtLiInsp.Text), Convert.ToInt32(txtLsInsp.Text));
 
 
-            List<RkRow> tablaRk = simulator.CalcularRK(Convert.ToInt32(txtRk1.Text), Convert.ToDouble(txtRk2.Text), Convert.ToInt32(txtRk3.Text), Convert.ToDouble(txtHRK.Text), Convert.ToInt32(txtLsInsp.Text));
-            dgvRK.DataSource = tablaRk;
-            List<object> vectores = simulator.Simular(Convert.ToInt32(tiempo.Text), Convert.ToInt32(iteraciones.Text), 
-                Convert.ToInt32(i.Text), Convert.ToInt32(j.Text));
-                
-            dataGridViewResultados.DataSource = vectores;
-            lblPNoReparado.Text = simulator.ObtenerPNoReparado().ToString();
-            ocupAy.Text = simulator.ObtenerPorcOcupacionAyudante().ToString();
-            ocupRel.Text = simulator.ObtenerPorcOcupacionRelojero().ToString();
+                    List<RkRow> tablaRk = simulator.CalcularRK(Convert.ToInt32(txtRk1.Text), Convert.ToDouble(txtRk2.Text), Convert.ToInt32(txtRk3.Text), Convert.ToDouble(txtHRK.Text), Convert.ToInt32(txtLsInsp.Text));
+                    dgvRK.DataSource = tablaRk;
+                    List<object> vectores = simulator.Simular(Convert.ToInt32(tiempo.Text), Convert.ToInt32(iteraciones.Text),
+                        Convert.ToInt32(i.Text), Convert.ToInt32(j.Text));
+
+                    dataGridViewResultados.DataSource = vectores;
+                    lblPNoReparado.Text = simulator.ObtenerPNoReparado().ToString();
+                    ocupAy.Text = simulator.ObtenerPorcOcupacionAyudante().ToString();
+                    ocupRel.Text = simulator.ObtenerPorcOcupacionRelojero().ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Ingrese valores correctos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Ingrese datos del tipo adecuado en los campos (int o float según corresponda)", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            
+
+
+
+
+
+            
         }
 
         private void label1_Click(object sender, EventArgs e)
